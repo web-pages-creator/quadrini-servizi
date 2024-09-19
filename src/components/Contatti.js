3c157ccb536113fa7af7e0a38a4bdb665070f1b6
PeterQuadrini <quadrinipietro@gmail.com>
2024-09-19 17:38:20 +0200

    commit

--- src/components/Contatti.js
+++ src/components/Contatti.js
@@ -1,23 +1,107 @@
+import { useState } from 'react';
 import CustomInput from './CustomInput';
 import './styles/Contatti.css';
+import ReCAPTCHA from 'react-google-recaptcha';
+import emailjs from 'emailjs-com';
+import { useToast } from '@chakra-ui/react'
 
 function Contatti() {
+  const toast = useToast()
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [email, setEmail] = useState('')
+  const [societa, setSocieta] = useState('')
+  const [mex, setMex] = useState('')
+  const [privacy, setPrivacy] = useState(false)
+  const [chaptVal, setChaptVal] = useState(null)
+  const [error, setError] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+  const [emailSent, setEmailSent] = useState(false)
+
+  const handleInputChange = (val, updateFunction) => {
+    updateFunction(val)
+  }
+
+  const handleSubmit = (e) => {
+    if (emailSent) {
+      return
+    }
+    e.preventDefault();
+    const allRequiredTextFieldsFilledIn = name != '' && phone != '' && email != '' && mex != ''
+    const allRequiredBooleanFieldsChecked = privacy
+    let missingFields = []
+    if (name == '') missingFields.push('Nome e Cognome')
+    if (phone == '') missingFields.push('Telefono')
+    if (email == '') missingFields.push('Email')
+    if (mex == '') missingFields.push('Messaggio')
+    if (!privacy) missingFields.push('Consenso Privacy')
+    if (chaptVal == null) missingFields.push('Google reCAPTCHA')
+    setErrorMessage(missingFields.join(', '))
+    if (allRequiredTextFieldsFilledIn && allRequiredBooleanFieldsChecked && chaptVal) {
+        setError(false)
+        sendEmail(e)
+    } else {
+      setError(true)
+    }
+  }
+
+  function sendEmail(e) {
+    emailjs.send("service_irgqrg3","template_q8vhktp",{
+      from_name: name,
+      message: mex,
+      telefono: phone,
+      email: email,
+      societa: societa,
+      }, 'fPjEX6Gf3jcVjZ1ke')
+      .then(() => {
+        toast({
+          title: 'Email Inviata!',
+          description: "un operatore della nostra azienda la ricontatterà il prima possibile",
+          status: 'success',
+          duration: 5000,
+          position: 'top',
+          isClosable: true,
+        })
+        setEmailSent(true)
+      }, (err) => {
+        toast({
+          title: 'Errore',
+          description: "Non siamo riusciti ad inviare la mail, riprova più tardi: " + err,
+          status: 'error',
+          duration: 5000,
+          position: 'top',
+          isClosable: true,
+        })
+      });
+  }
+
   return (
-    <div className="Contatti">
+    <form className="Contatti" onSubmit={handleSubmit}>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
         <div className='left'>
           <div className='left-title'>Scrivici subito attraverso questo modulo per richiededere gratuitamente informazioni o un preventivo.</div>
           <div className=''><strong>Sede legale:</strong><br/>Via Carpi, 7 - 00071 - Pomezia (RM)<br />Tel: +39 3403580930<br />Email: info@quadriniservizi.it</div>
         </div>
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4 form">
-          <CustomInput type='text' label='Nome e Cognome *' />
-          <CustomInput type='text' label='Nummero di telefono *' />
-          <CustomInput type='text' label='Email *'/>
-          <CustomInput type='text' label='Società (facoltativo)'/>
-          <CustomInput className='col-span-3' type='textarea' label='* MESSAGGIO *'/>
+          <CustomInput type='text' label='Nome e Cognome *' onChangeProperty={(val) => handleInputChange(val.target.value, setName)}/>
+          <CustomInput type='text' label='Numero di telefono *' onChangeProperty={(val) =>handleInputChange(val.target.value, setPhone)}/>
+          <CustomInput type='text' label='Email *' onChangeProperty={(val) => handleInputChange(val.target.value, setEmail)}/>
+          <CustomInput type='text' label='Società (facoltativo)' onChangeProperty={(val) => handleInputChange(val.target.value, setSocieta)}/>
+          <CustomInput type='textarea' label='* MESSAGGIO *' onChangeProperty={(val) => handleInputChange(val.target.value, setMex)}/>
+          <div className='privacy md:col-span-2 col-span-1'>
+            <input type='checkbox' className='checkbox' onChange={(val) => handleInputChange(val.target.checked, setPrivacy)}/>
+            <h6>* Acconsento al trattamento dei miei dati personali in conformità alle vigenti norme sulla privacy. Dichiaro di aver letto e accettato l'informativa sulla privacy</h6>
+          </div>
+          <ReCAPTCHA sitekey='6LeiOUkqAAAAAMeuR7-w3_0uJXTeM3Q21-oj11G5' onChange={(val) => handleInputChange(val, setChaptVal)}></ReCAPTCHA>
+          {error && <div className='md:col-span-2 col-span-1 error'>
+            Non hai compilato i seguenti campi: {errorMessage}
+          </div> }
+          <div className='submit md:col-span-2 col-span-1' onClick={handleSubmit}>
+            Invia
+          </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
 
--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -5,12 +5,12 @@ function CustomInput(props) {
     <>
      {props.type == 'textarea' && 
       <div className="CustomInput md:col-span-2">
-        <textarea type='text' placeholder={props.label} className='custom-input textarea'></textarea>
+        <textarea type='text' placeholder={props.label} className='custom-input textarea' onChange={props.onChangeProperty}></textarea>
       </div>
       }
       {props.type == 'text' && 
       <div className="CustomInput">
-         <input type='text' placeholder={props.label} className='custom-input'></input>
+         <input type='text' placeholder={props.label} className='custom-input' onChange={props.onChangeProperty}></input>
       </div>
       }
     </>
